7c27d93d7f7db8eaae30fd2906abd11e01a570d6
Ricky Johnson <ricky.johnson@techsoft3d.com>
2023-10-03 12:36:11 -0400

    upgraded to caas

--- javascripts/conversionService.js
+++ javascripts/conversionService.js
@@ -1,27 +1,17 @@
 modelUIDs = [
-        "26b01832-cde1-49a8-b5f3-b372e7d3d852", //fea
-        "51b118e4-dbe1-47f0-af44-4b43f4e79056", //VoiturePiston
-        "a3d6ef8d-5356-4fa6-bede-f45158b3fe95" //01-2_engine
+        "6fe5adad-d6e7-4b01-b2ec-64876b7fc26b", //fea
+        "4b9151f1-6f88-4635-a28b-28ebce9a94bd", //VoiturePiston
+        "d9279efa-2620-442b-98e5-e04061e4fd99" //01-2_engine
 ]
 
 async function startViewer(modelName) {
-        const conversionServiceURI = "https://csapi.techsoft3d.com";
-
         var viewer;
-
-        let res = await fetch(conversionServiceURI + '/api/streamingSession');
-        var data = await res.json();
-        var endpointUriBeginning = 'ws://';
-
-        if(conversionServiceURI.substring(0, 5).includes("https")){
-                endpointUriBeginning = 'wss://'
-        }
-
-        await fetch(conversionServiceURI + '/api/enableStreamAccess/' + data.sessionid, { method: 'put', headers: { 'items': JSON.stringify(modelUIDs) } });
-
+        let sessioninfo = await caasClient.getStreamingSession();
+        await caasClient.enableStreamAccess(sessioninfo.sessionid, modelUIDs);
+                
         viewer = new Communicator.WebViewer({
                 containerId: "viewer-container",
-                endpointUri: endpointUriBeginning + data.serverurl + ":" + data.port + '?token=' + data.sessionid,
+                endpointUri: sessioninfo.endpointUri,
                 model: modelName,
                 streamingMode: Communicator.StreamingMode.OnDemand,
                 boundingPreviewMode: "none",
@@ -36,16 +26,11 @@ async function startViewer(modelName) {
 }
 
 async function fetchVersionNumber() {
-        const conversionServiceURI = "https://csapi.techsoft3d.com";
-
-        let res = await fetch(conversionServiceURI + '/api/hcVersion');
-        var data = await res.json();
-        versionNumer = data;
-        
+        let data = await caasClient.getHCVersion();
+        versionNumer = data;        
         return data
-
 }
-
+      
 
 
 async function initializeViewer() {
